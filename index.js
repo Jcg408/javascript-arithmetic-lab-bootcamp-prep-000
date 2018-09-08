dd06a97e247d888d3ec9aa52bcb8472027b831cc
Jessica Gladstone <jcg321@comcast.net>
2018-09-08 14:53:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,16 @@ function dec(n) {
   return n -=1;
 }
 function makeInt (n) {
-  
   if(n === isNaN) {
     return true;
    }
-  else if (n) {
+  else {
     return parseInt(n, 10)
+  }
+}
+function preserveDecimal(n) {
+  if (n === isNaN) {
+    return true;
+    
   }
 }
