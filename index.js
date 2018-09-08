d2beaed9cccf7f00dd2fecb9a28a7fb645672f79
Jessica Gladstone <jcg321@comcast.net>
2018-09-08 14:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ function dec(n) {
   return n -=1;
 }
 function makeInt (n) {
-  parseInt(n);
   if(n === isNaN) {
     return true;
-   
+  }
+  if (n % 2 == 0) {
+    return parseInt(n);
   }
 }
